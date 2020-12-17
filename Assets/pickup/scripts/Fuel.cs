e41a9ff01293996a732fb826e18cec0a8b20f1b5
23hitth <23hitth@live.elida.k12.oh.us>
2020-12-17 13:05:56 -0500

    yeet

---
+++ Assets/pickup/scripts/Fuel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuel : MonoBehaviour
+{
+
+    [Header("Info")]
+    public float current = 100f;
+
+    [Header("Settings")]
+    public float max = 100f;
+    public float burnRate = 1f;
+
+    void Update()
+    {
+        current -= burnRate * Time.deltaTime;
+
+        if (current <= 0f)
+        {
+
+        }
+    }
+
+    void Refuel()
+    {
+        current = max;
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Final
+ My offroad project
+
+(11/16/2020) -Hunter-
+Basic terrain layout set up.
+
+(11/17/2020) -Hunter-
+Basic terrain setup finished.
+
+(11/18/2020 - 11/20/2020) -Hunter-
+placed ramp position and end position.
+
+(11/23/2020 - 11/24/2020) -Hunter-
+building terrain design and added textures.
